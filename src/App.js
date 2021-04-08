e1c7c8316de3bcf8df602e535ba6ea0000bd4b94
Lario <osunkunleolanrewaju@gmail.com>
2021-04-08 21:38:27 +0100

    added the main card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import {Grid} from '@material-ui/core'
 
-import Details from './components/Details'
+import Details from './components/Details/Details'
 import useStyles from './styles'
+import Main from './components/Main/Main'
 
 
 const App = () => {
@@ -14,8 +15,8 @@ const App = () => {
         <Grid item xs={12} sm={4}>
           <Details title="Income" />
         </Grid>
-        <Grid item xs={12} sm={4}>
-          Main
+        <Grid item xs={12} sm={3}>
+          <Main />
         </Grid>
         <Grid item xs={12} sm={4}>
           <Details title="Expense" />
