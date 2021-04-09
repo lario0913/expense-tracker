96ee916a06fbe9ddd7b61438880a9e58c24a3880
Lario <osunkunleolanrewaju@gmail.com>
2021-04-09 20:18:00 +0100

    form layout added

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Card, CardHeader, CardContent, Typography, Grid, Divider} from '@material-ui/core'
 
 import useStyles from './styles'
+import Form from './Form/Form'
 
 const Main = () => {
     const classes = useStyles()
@@ -10,11 +11,13 @@ const Main = () => {
             <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
             <CardContent>
                 <Typography align="center" variant="h5" >Total Balance: $100</Typography>
-                <Typography variant="subtitle1" style={{lineHeight: '1.5em', marginTop: '20px'}}>
+                <Typography variant="subtitle2" style={{lineHeight: '1.5em', marginTop: '20px'}}>
                     {/* Infocard */}
                     Try Saying: Add income for $100 in category salary for Monday
                 </Typography>
+
                 <Divider />
+                <Form />
             </CardContent>
             <CardContent className={classes.cardContent}>
                 <Grid container spacing={2}>
